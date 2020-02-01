a65c1268353080674957c3b6ece87443003512b1
Damilola <omifaredammy@gmail.com>
2020-02-01 22:57:09 +0000

    Add Sample 2

--- example/for_loop.py
+++ example/for_loop.py
@@ -1,3 +1,4 @@
+# Sample 1 
 my_list = [
     6, "Lisbon", "new york", 
     ["This is now", "Hello", 123],
@@ -7,3 +8,11 @@ for i in (my_list):
     # my_list is the iterable. 
     # 6 is the first element and expected to be printed first. 
     print(i)
+
+# Sample 2 
+another_list = [1,2,3, ["this is a test"]]
+for i in another_list:
+    if type(i) is list:
+        print(f'{i} found.')        
+    else:
+        print(f'No list in {another_list}')
